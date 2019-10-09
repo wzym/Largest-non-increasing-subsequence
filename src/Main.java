fac43288522e84da72d1b10676613857dc2b64db
wzym@yandex.ru <Afdqqq*___92>
2019-10-09 13:50:44 +0300

    It works.

--- src/Main.java
+++ src/Main.java
@@ -30,7 +30,45 @@ public class Main {
     }
 
     private int[] findNonIncreasing(Integer[] arrToSearch) {
+        int[] dyn = new int[arrToSearch.length + 1];
+        Arrays.fill(dyn, Integer.MIN_VALUE);
+        dyn[0] = Integer.MAX_VALUE;
+        int[] posOfTails = new int[arrToSearch.length + 1];
+        int[] ancestors = new int[arrToSearch.length];
 
-        return new int[0];
+
+        for (int i = 0; i < arrToSearch.length; i++) {
+            int j = getFirstLess(dyn, arrToSearch[i]);
+            dyn[j] = arrToSearch[i];
+            posOfTails[j] = i;
+            ancestors[i] = posOfTails[j - 1];
+        }
+
+        int length = dyn.length - 1;
+        for (; length >= 0; length--) {
+            if (dyn[length] > Integer.MIN_VALUE) break;
+        }
+        int minIndex = posOfTails[length];
+
+        int[] result = new int[length];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = minIndex + 1;
+            minIndex = ancestors[minIndex];
+        }
+        return result;
+    }
+
+    private int getFirstLess(int[] arr, Integer value) {
+        int l = 0;
+        int r = arr.length - 1;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (arr[m] >= value) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return l;
     }
 }
